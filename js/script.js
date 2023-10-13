198ce844027af524713f4e57aac09e8a89224e15
Antonio Ruiz <3speedemon3@gmail.com>
2023-10-13 14:40:36 +0200

    obteniendo numero de enlaces

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/**
+ * A partir de la página web proporcionada y utilizando las funciones DOM, mostrar por pantalla la siguiente información:
+
+    Número de enlaces de la página
+    Dirección a la que enlaza el penúltimo enlace
+    Numero de enlaces que enlazan a http://prueba
+    Número de enlaces del tercer párrafo
+
+ */
+
+// Numero de enlaces de la pagina
+console.log("Hello JS");
+
+let numTagsA = document.getElementsByName('a');
+
+console.log(numTagsA.length());
+
+
+// Direccion del penultimo enlace
+
+// Numero de enlaces que apuntan a http://prueba
+
+// Numero de enlaces del tercer p�rrafo
