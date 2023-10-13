2002aafa2c8f52eca42a6a6aef848cef7fd07f9c
Antonio Ruiz <3speedemon3@gmail.com>
2023-10-13 20:33:39 +0200

    Eliminando archivos

--- script3.js
+++
@@ -1,9 +0,0 @@
-/**
- * Cuando se pinche sobre el primer enlace, se oculte su sección relacionada
- * 
- * Cuando se vuelva a pinchar sobre el mismo enlace, se muestre otra vez esa sección de contenidos
- * 
- * Completar el resto de enlaces de la página para que su comportamiento sea idéntico al del primer enlace
- * 
- * Cuando una sección se oculte, debe cambiar el mensaje del enlace asociado (pista: propiedad innerHTML)
- */
